dd561d4d971dfb3b41d346d2ac5981a90780b968
prbhlnkr123 <prabhleen.sethi@thoughtworks.com>
2024-04-09 21:18:34 +0530

    Use %s for string formatting

--- api/src/decorators.py
+++ api/src/decorators.py
@@ -10,11 +10,11 @@ def log(func):
         args_repr = [repr(a) for a in args]
         kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
         signature = ", ".join(args_repr + kwargs_repr)
-        logger.debug("function {} called with args {}".format(func.__name__, signature))
+        logger.debug("function %s called with args %s",func.__name__, signature)
         try:
             result = func(*args, **kwargs)
             return result
         except Exception as e:
-            logger.exception("Exception raised in {}. exception: {}".format(func.__name__, str(e)))
+            logger.exception("Exception raised in %s. exception: %s",func.__name__, str(e))
             raise e
     return wrapper
