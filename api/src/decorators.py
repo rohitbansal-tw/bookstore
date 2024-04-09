6e351d7dfaaaf4ebc272bf43d7d7d0d75a10b4b1
prbhlnkr123 <prabhleen.sethi@thoughtworks.com>
2024-04-09 13:14:52 +0530

    Fix linting issues

--- api/src/decorators.py
+++ api/src/decorators.py
@@ -10,11 +10,11 @@ def log(func):
         args_repr = [repr(a) for a in args]
         kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
         signature = ", ".join(args_repr + kwargs_repr)
-        logger.debug(f"function {func.__name__} called with args {signature}")
+        logger.debug("function {} called with args {}".format(func.__name__, signature))
         try:
             result = func(*args, **kwargs)
             return result
         except Exception as e:
-            logger.exception(f"Exception raised in {func.__name__}. exception: {str(e)}")
+            logger.exception("Exception raised in {}. exception: {}".format(func.__name__, str(e)))
             raise e
     return wrapper
