119a8f7cc5c2cc647abdd64120c9224cdf6df4f2
prbhlnkr123 <prabhleen.sethi@thoughtworks.com>
2024-04-09 12:28:37 +0530

    Added log decorator

---
+++ api/src/decorators.py
@@ -0,0 +1,20 @@
+import functools
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        logger.debug(f"function {func.__name__} called with args {signature}")
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except Exception as e:
+            logger.exception(f"Exception raised in {func.__name__}. exception: {str(e)}")
+            raise e
+    return wrapper
